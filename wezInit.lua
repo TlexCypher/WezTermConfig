0ef471772fff5040a6734c18b6ec9010423a8089
Taichi Araki <araki130710@gmail.com>
2024-11-23 17:38:26 +0900

    add blur config and new color schema

--- wezInit.lua
+++ wezInit.lua
@@ -3,6 +3,7 @@ local module = {}
 function module.apply_to_config(config)
     config.scrollback_lines = 3500
     config.enable_scroll_bar = true
+    config.macos_window_background_blur = 12
 end
 
 return module
