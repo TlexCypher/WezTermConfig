058c1476ac03bdeb900aa04c68ca85d17dee6984
Taichi Araki <araki130710@gmail.com>
2024-12-12 00:17:58 +0900

    update color

--- wezColorScheme.lua
+++ wezColorScheme.lua
@@ -1,7 +1,8 @@
 local module = {}
 
 function module.apply_to_config(config)
-    config.color_scheme = "tokyonight"
+    -- config.color_scheme = "Batman"
+    -- config.color_scheme = "tokyonight"
     -- config.color_scheme = "Tomorrow Night"
     -- config.color_scheme = "Monokai (dark) (terminal.sexy)"
     -- config.color_scheme = "Solarized Dark"
